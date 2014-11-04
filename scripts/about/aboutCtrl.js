3b63a9eafe39a0fce5bc7aa6eea1c315aac00168
Cuong Nguyen <nvcuong.infotech@yahoo.com>
2014-11-05 00:43:55 +0700

    finish about page

--- scripts/about/aboutCtrl.js
+++ scripts/about/aboutCtrl.js
@@ -9,25 +9,17 @@
       fields: []
     };
 
-    ctrl.basicInfo.fields.push({
-      label: 'Gender',
-      value: 'Male'
-    });
+    ctrl.basicInfo.fields.push({ label: 'Gender', value: 'Male' });
+    ctrl.basicInfo.fields.push({ label: 'Looking for', value: 'Friends, Dating, A relationship, Networking' });
+    ctrl.basicInfo.fields.push({ label: 'Birthday', value: 'October 8' });
+    ctrl.basicInfo.fields.push({ label: 'Relationship', value: 'Single' });
 
-    ctrl.basicInfo.fields.push({
-      label: 'Looking for',
-      value: 'Friends, Dating, A relationship, Networking'
-    });
-
-    ctrl.basicInfo.fields.push({
-      label: 'Birthday',
-      value: 'October 8'
-    });
-
-    ctrl.basicInfo.fields.push({
-      label: 'Relationship',
-      value: 'Single'
-    });
+    ctrl.langInfo = {
+      header: 'Languages',
+      fields: []
+    };
+    ctrl.langInfo.fields.push({ label: 'English', value: 'Professional working proficiency' });
+    ctrl.langInfo.fields.push({ label: 'Vietnamese', value: 'Native or bilingual proficiency' });
   }
 
   AboutCtrl.$inject = [];
--- scripts/mainModule.js
+++ scripts/mainModule.js
@@ -10,10 +10,10 @@
 
   function MainCtrl() {
     var ctrl = this;
-//    ctrl.myName = 'Nguyễn Vũ Cương';
-    ctrl.myName = '...';
-    ctrl.facebookLink = '';
-    ctrl.googlePlusLink = '';
+    ctrl.myName = 'Nguyễn Vũ Cương';
+//    ctrl.myName = '...';
+    ctrl.facebookLink = 'https://www.facebook.com/nvcuong.infotech';
+    ctrl.googlePlusLink = 'https://plus.google.com/u/1/+V%C5%A9C%C6%B0%C6%A1ngNguy%E1%BB%85n0/about';
     ctrl.navMenus = [
       { name: 'About', tpl: 'scripts/about/template.html'},
       { name: 'Skills', tpl: 'scripts/skills/template.html'},
